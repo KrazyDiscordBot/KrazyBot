5947ed49738b3526acffca0ea337dc174771d532
KrazyDev <67275060+KartikeSingh@users.noreply.github.com>
2021-11-25 20:55:25 +0530

    🎗 Small Typo Fixes 🎗

--- server.js
+++ server.js
@@ -3,17 +3,8 @@ require('dotenv').config();
 const KrazyBot = require('./utility/client');
 const Discord = require("discord.js");
 
-const myIntent = new Discord.Intents(Discord.Intents.ALL);
-myIntent.remove(['DIRECT_MESSAGE_TYPING', 'GUILD_MESSAGE_TYPING', 'DIRECT_MESSAGES', 'DIRECT_MESSAGE_REACTIONS', 'GUILD_INTEGRATIONS']);
-
-new KrazyBot({ partials: ["REACTION", "MESSAGE"], intents: myIntent }).on("messageUpdate");
+new KrazyBot({ partials: ["REACTION", "MESSAGE"], intents: ["GUILDS", "GUILD_MESSAGES", "GUILD_MESSAGE_REACTIONS", "DIRECT_MESSAGES", "DIRECT_MESSAGE_REACTIONS", "GUILD_MEMBERS", "GUILD_BANS", "GUILD_VOICE_STATES"] })
 
 const mongoose = require("mongoose");
 
 mongoose.connect(process.env.MONGO_URI, { useUnifiedTopology: true, useNewUrlParser: true, useCreateIndex: true }).catch((e) => console.log(e));
-
-const app = require("express")();
-
-app.get('/', (req, res) => res.send(200));
-
-app.listen(process.env.PORT || 3000);
